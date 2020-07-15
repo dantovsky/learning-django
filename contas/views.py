1f7f6b237cd6f40063a68b7b89030961be04e2af
Dante Marinho <dantecnia@hotmail.com>
2020-07-15 14:53:37 +0100

    Models and migrations. Rendering and sending data to template using Jinja: using variables and programming operations inside tamplate HTML.

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from .models import Transacao
 
 # Create your views here.
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
 
-    return render(request, 'contas/home.html') # HttpResponse(html)
+    return render(request, 'contas/home.html', data)  # HttpResponse(html)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()  # objects é um Manager
+    return render(request, 'contas/listagem.html', data)
+
+# Manager :: uma classe que o Django implementa para todos os models, trazendo operacoes de BD:
+# all(), filter(), last(), first().
