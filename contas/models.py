74678417dc41233375a471ef079bab6851bf6d34
Dante Marinho <dantecnia@hotmail.com>
2020-07-18 05:20:51 +0100

    Small change.

--- contas/models.py
+++ contas/models.py
@@ -14,7 +14,7 @@ class Transacao(models.Model):
     data = models.DateTimeField()  # auto_now=True | auto_now_add=True
     descricao = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # A transacao avi poder ter 1 Categoria, mas a Categoria pode estar em varias transacoes
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # A transacao vai poder ter 1 Categoria, mas a Categoria pode estar em varias transacoes
     observacoes = models.TextField(null=True, blank=True)  # campo de texto
 
     class Meta:
